ba836fd9bd1497c6d3f66bc4cca31197d3f2cdb3
cybersagittarius <cybersagittarius@gmail.com>
2021-04-08 19:46:02 +0200

    added routers

---
+++ routers/contactUsRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const contactUsController = require('../controllers/contactUsController')
+
+router.post('/', (req, res)=>{
+
+
+})
+
+module.exports = router;
