ec48e75550e730af4cf718de9ce1d437b6cf2111
cybersagittarius <cybersagittarius@gmail.com>
2021-04-14 18:25:14 +0200

    added mongoose

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
 const express = require('express');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(logger('dev'));
+app.use(cors());
+
+const uri = process.env.ATLAS_MONGO_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+
+const connection = mongoose.connection;
+connection.once('open', ()=>{
+    console.log('MongoDB connection established successfully!')
+})
 
 const registerRouter = require('./routes/registerRouter');
 const loginRouter = require('./routes/loginRouter');
 const pwResetRouter = require('./routes/pwResetRouter');
 const contactUsRouter = require('./routes/contactUsRouter');
 
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(logger('dev'));
-
 app.use('/register', registerRouter);
 app.use('/login', loginRouter);
 app.use('/reset_password', pwResetRouter);
 app.use('/contact', contactUsRouter);
 
-const PORT = process.env.PORT || 4000
-app.listen(PORT, (err)=>{
+app.listen(port, (err)=>{
     if(err){
         console.log(err);
         return err;
     }else{
-        console.log(`Application now listening on port ${PORT}`);
+        console.log(`Application/Server is now listening on port ${port}`);
     }
 })
