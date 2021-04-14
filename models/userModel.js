776e4d66292dd27596abdfd49dc5fc83034aae6e
cybersagittarius <cybersagittarius@gmail.com>
2021-04-14 21:38:11 +0200

    started hashed password implementation

--- models/userModel.js
+++ models/userModel.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
-const Schema = mongoose.Schema;
-
-const userSchema = new Schema ({
-    username: { }
+const userSchema = new mongoose.Schema ({
+    username: { type: String, required: true },
+    password: { type: String, required: true},
 })
 
 
