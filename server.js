77197c655e4671dc5173ac23300ba23f95e8192b
cybersagittarius <cybersagittarius@gmail.com>
2021-04-15 09:33:14 +0200

    added comments

--- server.js
+++ server.js
@@ -13,19 +13,23 @@ app.use(express.urlencoded({extended: false}));
 app.use(logger('dev'));
 app.use(cors());
 
+//set uri for database
 const uri = process.env.ATLAS_MONGO_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
 
+//connect to database
 const connection = mongoose.connection;
 connection.once('open', ()=>{
     console.log('MongoDB connection established successfully!')
 })
 
+//require routers
 const registerRouter = require('./routes/registerRouter');
 const loginRouter = require('./routes/loginRouter');
 const pwResetRouter = require('./routes/pwResetRouter');
 const contactUsRouter = require('./routes/contactUsRouter');
 
+//user routers as middlewares
 app.use('/register', registerRouter);
 app.use('/login', loginRouter);
 app.use('/reset_password', pwResetRouter);
