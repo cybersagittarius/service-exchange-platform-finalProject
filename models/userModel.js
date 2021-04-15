6993cd14883b76eeb1b89b169002ca1d72834cbe
cybersagittarius <cybersagittarius@gmail.com>
2021-04-15 10:44:14 +0200

    created 1st draft for user model

--- models/userModel.js
+++ models/userModel.js
@@ -2,10 +2,51 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema ({
-    username: { type: String, required: true },
-    password: { type: String, required: true},
+    username: { type: String, required: true, unique: true, minlength: 3, trim: true },
+    password: { type: String, required: true, minlength: 8, maxlength: 12 },
+
+},
+    {timestamps: true}
+)
+
+//pre middleware
+userSchema.pre('save', (next)=>{
+    //constructor
+    const user = this
+
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.getSalt(10, (saltError, salt) =>{
+            if(saltError) {
+                //??
+                console.log('Error!')
+                return next(saltError)
+            } else {
+                bcrypt.hash(user.password, salt, (hashError, hash)=>{
+                    if(hashError) {
+                        return next(hashError)                        
+                    } 
+
+                    user.password = hash;
+                    //part of the syntax of middleware, which means to move on to the next function
+                    next()
+                })
+            }
+        })
+    } else {
+        //?? why
+        return next()
+    }
 })
 
+userSchema.methods.comparePassword = (password, callback) =>{
+    bcrypt.compare(password, this.password, (error, isMatch)=>{
+        if(error) {
+            return callback(error)
+        } else {
+            callback(null, isMatch)
+        }
+    })
+}
 
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+//this module will export User as the instance 'User' of userSchema
+module.exports = mongoose.model('User', userSchema);
