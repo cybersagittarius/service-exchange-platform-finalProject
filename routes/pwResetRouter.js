3185ff07b5b2e26d8469935ed720f971de1b0e3e
cybersagittarius <cybersagittarius@gmail.com>
2021-04-21 20:19:15 +0200

    commented out get method

--- routes/pwResetRouter.js
+++ routes/pwResetRouter.js
@@ -2,24 +2,24 @@ const express = require('express');
 const router = express.Router();
 const pwResetController = require('../controllers/pwResetController');
 
-const path = require('path');
-const fs = require('fs');
-const usersPath = path.join(__dirname, '..', 'model', 'users.json');
+// const path = require('path');
+// const fs = require('fs');
+// const usersPath = path.join(__dirname, '..', 'model', 'users.json');
 
-router.get('/', (req, res)=>{
-    const body = req.body;
+// router.get('/', (req, res)=>{
+//     const body = req.body;
 
-    fs.readFile(usersPath, (err, data)=>{
-        if(err){
-            throw err
-        }else{
-            const newData = JSON.parse(data)
-            res.send(newData)
+//     fs.readFile(usersPath, (err, data)=>{
+//         if(err){
+//             throw err
+//         }else{
+//             const newData = JSON.parse(data)
+//             res.send(newData)
 
             //if we do not parse data and just use res.send(data), on browser the data will be downloaded to a file
-        }
-    })
-})
+//         }
+//     })
+// })
 
 router.post('/', (req, res)=>{
     
