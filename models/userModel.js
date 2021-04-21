327b676de1f79abfa65764105e538343cbc6fab1
cybersagittarius <cybersagittarius@gmail.com>
2021-04-21 15:52:20 +0200

    added a document but deleted later

--- models/userModel.js
+++ models/userModel.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema ({
     region: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true } 
+    password: { type: String, required: true }, 
 },
     {timestamps: true}
 )
