fb0790f34b61c5cd398670eae4b5d776de4029a1
cybersagittarius <cybersagittarius@gmail.com>
2021-04-22 16:49:56 +0200

    changed the file name to dbConnect

---
+++ config/dbConnect.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+//without async
+//mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+
+//connect to database
+// const connection = mongoose.connection;
+// connection.once('open', (err)=>{
+//     if(err){
+//         console.log('cannot open the connection!')
+//     }
+//     console.log('MongoDB connection established successfully!')
+// })
+
+//implement async
+const connectDB = async () => {
+    //set uri for database
+    const uri = process.env.MONGO_ATLAS_URI;
+    try {
+        const conn = await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useFindAndModify: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true
+        });
+        console.log(`MongoDB connected to Atlas ${conn.connection.host}`);
+    }catch(err){
+        return console.log(err)
+    }
+}
+
+module.exports = connectDB;
