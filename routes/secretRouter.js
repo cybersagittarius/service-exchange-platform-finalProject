c62d559798ef26d131d8b07b2c52e24c3f1f334c
cybersagittarius <cybersagittarius@gmail.com>
2021-04-27 15:37:46 +0200

    removed next argument

--- routes/secretRouter.js
+++ routes/secretRouter.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const token = require('../token/tokens')
 
-router.get ('/', token.tokenCheck, (req, res, next) =>{
+router.get ('/', token.tokenCheck, (req, res) =>{
     res.json({
         message: 'You got the secret from me! Open Sesame!',
         cookies: req.cookies
     })
-    .then(res.redirect('/profile'))
+    // we do not redirect from one page to another in the backend
+    // we do so in the frontend
+    //.then(res.redirect('/profile'))
     .catch((err) => console.log(err))
-
-    next();
 })
 
 module.exports = router 
