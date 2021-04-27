36c5cb00588ff772fdf678554c07dccfba0c406b
Maja <majamarek.design@gmail.com>
2021-04-27 09:34:20 +0200

    menu ready for back-end

--- client/src/Components/Find.jsx
+++ client/src/Components/Find.jsx
@@ -1,136 +1,15 @@
 import React, { useState } from "react";
 import { CountryDropdown, RegionDropdown } from "react-country-region-selector";
-import Offer from "./Offer";
-/* import DisplayedSelection from './DisplayedSelection'; */
-/* import LookingFor from "./LookingFor"; */
-
-const items = [
-  {
-    id: 1,
-    value: "Animals",
-  },
-  {
-    id: 2,
-    value: "Archeology",
-  },
-  {
-    id: 3,
-    value: "Astronamy",
-  },
-  {
-    id: 4,
-    value: "Biology",
-  },
-  {
-    id: 5,
-    value: "Books",
-  },
-  {
-    id: 6,
-    value: "Chemistry",
-  },
-  {
-    id: 7,
-    value: "Cooking",
-  },
-  {
-    id: 8,
-    value: "Design",
-  },
-  {
-    id: 9,
-    value: "Electrochemistry",
-  },
-  {
-    id: 10,
-    value: "Engineering",
-  },
-  {
-    id: 11,
-    value: "Food",
-  },
-  {
-    id: 12,
-    value: "Geography",
-  },
-  {
-    id: 13,
-    value: "History",
-  },
-  {
-    id: 14,
-    value: "IT",
-  },
-  {
-    id: 15,
-    value: "House",
-  },
-  {
-    id: 16,
-    value: "Languages",
-  },
-  {
-    id: 17,
-    value: "Leisure Activities",
-  },
-  {
-    id: 18,
-    value: "Mathematics",
-  },
-  {
-    id: 19,
-    value: "Mechanics",
-  },
-  {
-    id: 20,
-    value: "Medical scrience",
-  },
-  {
-    id: 21,
-    value: "Philosophy",
-  },
-  {
-    id: 22,
-    value: "Music",
-  },
-  {
-    id: 23,
-    value: "Philosophy",
-  },
-  {
-    id: 24,
-    value: "Physics",
-  },
-  {
-    id: 25,
-    value: "Plants",
-  },
-  {
-    id: 26,
-    value: "Psychology",
-  },
-  {
-    id: 27,
-    value: "Science",
-  },
-  {
-    id: 28,
-    value: "Spirituality",
-  },
-  {
-    id: 29,
-    value: "Social studies",
-  },
-  {
-    id: 30,
-    value: "Sport",
-  },
-];
+import SkillsMenu from "./SkillsMenu";
+import FoundProfiles from './FoundProfiles';
+import items from '../assets/js/itemsSkills.js'
+/* import DisplayedSelection from "./DisplayedSelection"; */
 
 function Find() {
   const [country, setCountry] = useState(" ");
   const [region, setRegion] = useState(" ");
-  const [Items, setItems] = useState(" ");
+  const [offerSelection, setofferSelection] = useState([]);
+ const [lookSelection, setlookSelection] = useState([]);
 
   const selectCountry = (e) => {
     setCountry(e);
@@ -140,29 +19,44 @@ function Find() {
     setRegion(e);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setCountry(" ");
-    setRegion(" ");
-  };
-  const selectItems = (e) => setItems(e);
+  const handleOfferSelection = (selection) => {
+    setofferSelection(selection)
+  }
+
+  const handlelookSelection = (selection) => {
+    setlookSelection(selection)
+  }
+
+  const handleFind = () => {
+    //This function send info to the backend
+  }
 
   return (
-    <form onSubmit={handleSubmit} className="form">
-      <Offer title="I can offer" items={items} multiSelect />
-      {/* <DisplayedSelection selected={selected}/> */}
-      <Offer title="I'm looking for" items={items} multiSelect />
-      {/* <LookingFor value={items} onChange={(e) => selectItems(e)} />
-      <div className="menu"> */}
-        <CountryDropdown value={country} onChange={(e) => selectCountry(e)} />
-        <RegionDropdown
-          country={country}
-          value={region}
-          onChange={(e) => selectRegion(e)}
-        />
-      
+    <form className="form">
+    {/* <DisplayedSelection listOfSelection={offerSelection} /> */}
+      <SkillsMenu
+        title="I can offer"
+        items={items}
+        multiSelect
+        selection={offerSelection}
+        handleSelection={handleOfferSelection}
+      />
+      {/* <DisplayedSelection listOfSelection={lookSelection} /> */}
+      <SkillsMenu 
+      title="I'm looking for" 
+      items={items} 
+      multiSelect
+      selection={lookSelection}
+      handleSelection={handlelookSelection}
+      />
+      <CountryDropdown value={country} onChange={(e) => selectCountry(e)} />
+      <RegionDropdown
+        country={country}
+        value={region}
+        onChange={(e) => selectRegion(e)}
+      />
       <div className="btnFindDiv">
-        <button type="submit" value="search" className="btnFind">
+        <button onclick= {handleFind} type="submit" value="search" className="btnFind" >
           Find
         </button>
       </div>
