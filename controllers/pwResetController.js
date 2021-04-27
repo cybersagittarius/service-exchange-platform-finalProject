821e9129192c1c40ff5eabd4903318a8f58e80aa
cybersagittarius <cybersagittarius@gmail.com>
2021-04-27 12:56:38 +0200

    started working on nodemail and pw reset

--- controllers/pwResetController.js
+++ controllers/pwResetController.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/userModel')
-
+const crypto = require('crypto');
 const customError = require('../config/customError');
+const nodemailer = require('nodemailer');
 // const path = require('path')
 // const fs = require('fs')
 
@@ -18,6 +19,19 @@ const checkEmail = (body)=>{
             }else if(!user){
                 reject(new Error('No matching error!'))
                 }else{
+                    const token = crpto.randomBytes(20).toString('hex');
+                    user.update({
+                        pwResetToken: token,
+                        pwResetExpires: Date.now() + 3600000,
+                    });
+
+                    const transporter = nomailer.createTransport({
+                        service: 'gmail',
+                        auth: {
+                            user: `${process.env.EMAIL_ADDRESS}`,
+                            pass: `${process.env.EMAIL_PASSWORD}`,
+                        },
+                    });
                     resolve('Please check your email for a password recovery link.')
                 }
             }
