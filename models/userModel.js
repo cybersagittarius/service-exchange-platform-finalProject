46c2ec38903498e48a3a0b01c5e1760fe0b7fec6
cybersagittarius <cybersagittarius@gmail.com>
2021-04-29 21:18:44 +0200

    added avatar as part of userSchema

--- models/userModel.js
+++ models/userModel.js
@@ -11,7 +11,6 @@ const userSchema = new mongoose.Schema ({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true }, 
     avatar: { data: Buffer, contentType: String, required: true},
-    skills: {} 
 },
     {timestamps: true}
 )
