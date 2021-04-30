f881f447a04f78cebd76deac3eec61be4bfffe92
cybersagittarius <cybersagittarius@gmail.com>
2021-04-30 13:49:03 +0200

    included avatar in schema, 1st time failed

--- models/userModel.js
+++ models/userModel.js
@@ -11,6 +11,7 @@ const userSchema = new mongoose.Schema ({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true }, 
     avatar: { data: Buffer, contentType: String, required: true},
+    skills: { type: String, required: true },
 },
     {timestamps: true}
 )
