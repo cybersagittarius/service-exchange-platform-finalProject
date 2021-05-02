c1efa6a1d6fb879c11b107df4680a873cb9b6ed9
cybersagittarius <cybersagittarius@gmail.com>
2021-05-02 10:43:43 +0200

    usermodel revisited

--- models/userModel.js
+++ models/userModel.js
@@ -10,8 +10,8 @@ const userSchema = new mongoose.Schema ({
     email: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true }, 
-    avatar: { data: Buffer, contentType: String, required: true},
-    skills: { type: String, required: true },
+    // avatar: { data: Buffer, contentType: String, required: true},
+    // skills: { type: String, required: true },
 },
     {timestamps: true}
 )
