95d3c81e4d2034664d995a4c3d864e845981521c
cybersagittarius <cybersagittarius@gmail.com>
2021-05-05 22:08:37 +0200

    fixed {_id: id} to {id:_id}

--- controllers/deleteController.js
+++ controllers/deleteController.js
@@ -2,15 +2,14 @@ const User = require('../models/userModel')
 const express = require('express')
 
 const deleteUser = async(req, res, next)=>{ 
-      console.log(req.user)
+      console.log(req.body)
    try{
-       const user = await User.findOneAndDelete({_id: req.user.id})
-        res.json({msg: `The user ${req.user.name} has been deleted`})
+       const user = await User.findOneAndDelete({id: req.user._id})//important! id:_id
+        res.json({msg: `The user has been deleted`})
     } catch(err){
     next(err.message)  
   }    
 }
-
 
 module.exports = deleteUser
 
