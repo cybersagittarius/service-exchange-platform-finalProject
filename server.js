73690b511ef84d94670937c49ece7cb488bc4348
cybersagittarius <cybersagittarius@gmail.com>
2021-05-07 16:40:21 +0200

    added console.log in line 73 for err

--- server.js
+++ server.js
@@ -85,6 +85,7 @@ app.use((req, res, next) => {
 //Central Error Handling for internal server error 
 //added next to make sure whatever error is forwarded
 app.use(errorHandler = (err, req, res, next)=> {  
+    console.log(err);
     res.status(err.status || 500).json({ error: err.message })
 })
 
