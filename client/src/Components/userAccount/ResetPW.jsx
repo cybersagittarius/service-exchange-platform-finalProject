b789b9721d5e970f898b9aa3b2d62ef6bfd4f739
cybersagittarius <cybersagittarius@gmail.com>
2021-05-07 17:24:06 +0200

    added axios function

--- client/src/Components/userAccount/ResetPW.jsx
+++ client/src/Components/userAccount/ResetPW.jsx
@@ -7,38 +7,25 @@ const ResetPW = () => {
   const [email, setEmail] = useState("");
   const [alertEM, setAlertEM] = useState(false);
 
-  const postEmail = (email) => {
-     const capturedData = { email }
+  const postEmail = (email) =>{
 
-     axios.post('/reset_password', capturedData)
-     .then(response=>{ 
-      console.log('res received', response.data)
-      return response.json()
-    })
-    .then(oldEmailFound => console.log(oldEmailFound))
+    const data = { email }
+
+   axios.post('http://localhost:4000/reset_password', data)
+   .then(res => console.log(res.data))
+   .then(oldEmailFound => console.log(oldEmailFound))
   // in case the API responded, we will have the error inside error.response.data 
     .catch(error=>console.log(error.response && error.response.data))
-  }
-
-  //   //fetch data to send it to backend
-  //   fetch('http://localhost:4000/reset_password', {
-  //     method: "POST",
-  //     headers: {'Context-Type': 'application/json'},
-  //     body: JSON.stringify(data)
-  //     //in backend the data will be received in req.body
-  //   })
-  //   .then(res=>res.json())
-  //   .then(emailReceived=>console.log(emailReceived))
-  //   .catch(err=>console.log(err))
-  // }
+    .catch(err => console.log(err))
+  } 
 
   const submitHandler = (e) => {
     e.preventDefault();
 
     const emailValidator = /^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,15}$/;
-    const isEmValid = emailValidator.test(email);
+    const isEmailValid = emailValidator.test(email);
 
-    if (!isEmValid) {
+    if (!isEmailValid) {
       setAlertEM(true);
       setTimeout(() => {
         setAlertEM(false);
@@ -58,6 +45,7 @@ const ResetPW = () => {
   return (
     <>
       <ResetPWForm
+        postEmail = { postEmail }
         submitHandler = { submitHandler }
         changeEmail = { changeEmail }
         email = { email }
