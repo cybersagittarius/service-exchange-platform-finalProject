aad294a1f0a223fa48e7d32e03186ca214c643ab
cybersagittarius <cybersagittarius@gmail.com>
2021-05-08 11:47:41 +0200

    added an axios example but commented out

--- client/src/Components/Test.jsx
+++ client/src/Components/Test.jsx
@@ -19,14 +19,23 @@ const Test = () => {
                                    "Access-Control-Allow-Credentials": true}
                         }
 
-            axios.delete(`http://localhost:4000/delete`, config) 
+            axios.delete('http://localhost:4000/delete', config) 
             .then(res => {
             console.log(res);             
             console.log(res.data);       
             })
             .catch(err=>console.log(err))    
      }, [])
- 
+     
+    //  axios.post('http://localhost:4000/register', data)
+    //  .then(res=>{ 
+    //      console.log('res received', res.data)
+    //  })
+    //  .then(newUserCreated => console.log(newUserCreated))
+    //  // in case the API responded, we will have the error inside error.response.data 
+    //  .catch(err => console.log(err && err.response && err.response.data))
+    //  }  
+
     return (
         <div>
             <h1>What a doggy day!</h1>
