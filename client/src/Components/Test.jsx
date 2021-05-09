4c662763095c715c119ab63830280e8621227036
cybersagittarius <cybersagittarius@gmail.com>
2021-05-09 21:29:39 +0200

    commented out Test.jsx

--- client/src/Components/Test.jsx
+++ client/src/Components/Test.jsx
@@ -1,32 +1,33 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
+const Test = async() => {
 
-const Test = () => {
-
-    const [message, setMessage] = useState({});
+    //const [message, setMessage] = useState({});
     
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7I…5NDZ9.hf7j-k_jVAMRJxT3VWDS-Lz4sKDPvV7IclQbWrv1GK4"
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImVtYWlsIjoicG9vaEBnbWFpbC5jb20ifSwiaWF0IjoxNjIwNTc4MTEyLCJleHAiOjE2MjA1Nzg3MTJ9.bATM_A5qOVupROR7V_0hR8f9Nle6WT5-YE3plWZ8Zc4"
 
-    useEffect(() => {
+    //useEffect(() => {
         //    const config = { headers: {"Content-Type": "application/json", "Access-Control-Allow-Origin": "http://localhost:3000/", 
         //    "Access-Control-Allow-Credentials": true},
         //     }
             // const request = await axios.get('http://localhost:4000/delete')
             //setMessage(request)
+    
 
-        const config = { headers: {"Authorization": token, 
-                                   "Access-Control-Allow-Credentials": true}
-                        }
-
-            axios.delete('http://localhost:4000/delete', config) 
+        const config = { headers: {"Authorization": token }}
+                                   //"Access-Control-Allow-Credentials": true}
+                       
+        await axios.delete('http://localhost:4000/delete', config) 
             .then(res => {
-            console.log(res);             
-            console.log(res.data);       
+            //console.log(res)          
+            console.log('res received', res.data);       
             })
-            .catch(err=>console.log(err))    
-     }, [])
-     
+            .catch(err=>console.log(err))  
+          
+     //}, [])
+
+         
     //  axios.post('http://localhost:4000/register', data)
     //  .then(res=>{ 
     //      console.log('res received', res.data)
