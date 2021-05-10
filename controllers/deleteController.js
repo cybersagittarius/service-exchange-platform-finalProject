34f3e9c3810107d13bc6257f924294f1c479be63
cybersagittarius <cybersagittarius@gmail.com>
2021-05-10 08:20:33 +0200

    removed the email check implementation

--- controllers/deleteController.js
+++ controllers/deleteController.js
@@ -1,15 +1,38 @@
 const User = require('../models/userModel')
 const express = require('express')
 
-const deleteUser = async(req, res, next)=>{ 
+const deleteUser = async(req, res, next) => { 
       console.log(req.body)
-   try{
+     
+
+   try {
        const user = await User.findOneAndDelete({id: req.user._id})//important! id:_id
         res.json({msg: `The user ${ req.user.name } has been deleted`})
     } catch(err){
-    next(err.message)     
-  }    
-}
+    next(err.message) 
+    } 
+  }
+  
+  module.exports = deleteUser
 
-module.exports = deleteUser
+      // const [email] = req.body.emal
+      // const user = email
+
+      // let findUser = await User.findOne({email: user.email});
+      
+      // if(!findUser) {
+      //   return res.status(400).json({error: [{msg: "no user found!"}] 
+      //   })
+      // }
+      // if(findUser) {
+      //   await user.findOneAndDelete({id: findUser._id})
+      //   res.json({msg:`The user ${req.user.name} has been deleted!`})
+      // } 
+  //   }
+  // catch (err) {
+  //       return next(err.message)
+  //     }
+  // }  
+      
+    
 
