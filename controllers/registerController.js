50bd95351c31348b17d8a954275199d82280c7df
cybersagittarius <cybersagittarius@gmail.com>
2021-05-11 11:47:43 +0200

    no change, just a space

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -5,16 +5,6 @@ require('dotenv').config();
 const cloudinary = require('cloudinary').v2
 //const auth = require('../token/auth');
 
-    // firstname: { type: String, required: true },
-    // lastname: { type: String, required: true },
-    // country: { type: String, required: true },
-    // region: { type: String, required: true },
-    // email: { type: String, required: true, unique: true },
-    // username: { type: String, required: true },
-    // password: { type: String, required: true }, 
-//     // avatar: { data: Buffer, contentType: String, required: true},
-//     // skills: { type: String, required: true },
-
 const User = require('../models/userModel');
 // const check = check('firstname', 'firstname is required').not().isEmpty()
 
@@ -22,20 +12,19 @@ const registerUser = async(req, res, next) => {
   //get an new instance because this is for registration
    const user = new User(req.body);     
    //const {firstname, lastname, country, region, email, username } = user
-      
+   console.log(req.body);
+   
    try{
     let findUser = await User.findOne({email: user.email})
     if(findUser) {
-      return res.status(400).json({errors: [{msg: "email already registered"}]})
+      return res.status(400).json({error: [{msg: "email already registered"}]})
     }
      //not upload(user.savedImage) here, it has to come from the req.body
      const result = await cloudinary.uploader.upload(req.body.savedImage)
      user.avatar_url = result.secure_url
-     console.log(req.body.offerSelection)
-
+     
      //this line does not work 
-     const offerSelection = [];   
-     user.skills = offerSelection.push(req.body.offerSelection)
+  
      await user.save()
   }
   catch(err){
