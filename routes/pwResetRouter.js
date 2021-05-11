438b08504234f398f0cacb5aaa081e6cf23c3130
cybersagittarius <cybersagittarius@gmail.com>
2021-05-11 11:29:52 +0200

    deleted the files

--- routes/pwResetRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const pwResetController = require('../controllers/emailControllers/pwResetController')
-
-router.route('/').post(pwResetController)
-
-module.exports = router 
