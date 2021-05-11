98333dee52f9c25dbc78324a59f77a158c17deb5
cybersagittarius <cybersagittarius@gmail.com>
2021-05-11 11:42:55 +0200

    no change, maybe just a space

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 require ('dotenv').config();
 
-module.exports = function (req, res, next){
+const auth = (req, res, next) => {
     const authHeader = req.headers['authorization'] //there's a bearer token in here
     const token = authHeader && authHeader.split(' ')[1];
     
@@ -18,3 +18,5 @@ module.exports = function (req, res, next){
         res.status(401).json({msg: "token is not valid!"})
     }
 }
+
+module.exports = auth
