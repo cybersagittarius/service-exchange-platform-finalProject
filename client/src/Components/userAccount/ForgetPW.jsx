453e6312cde98e9faa0cd84fb5dbcea1af8b6ef9
cybersagittarius <cybersagittarius@gmail.com>
2021-05-12 15:54:26 +0200

    new files after being renamed from ResendPW to ForgetPW

---
+++ client/src/Components/userAccount/ForgetPW.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import axios from "axios";
+import ForgetPWForm from "./forms/ForgetPWForm";
+
+const ForgetPW = () => {
+  const [email, setEmail] = useState("");
+  const [alertEM, setAlertEM] = useState(false);
+
+  const postEmail = (email) =>{
+     const data = { email }
+    axios.post('http://localhost:4000/forget_password', data)
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err.message))
+   }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const emailValidator = /^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,15}$/;
+    const isEmValid = emailValidator.test(email);
+
+    if (!isEmValid) {
+      setAlertEM(true);
+      setTimeout(() => {
+        setAlertEM(false);
+      }, 5000);
+      return false;
+    }
+
+    postEmail(email); 
+    
+    //setEmail("");
+  };
+
+  const changeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  return (
+    <>
+      <ForgetPWForm
+        // postEmail = { postEmail }
+        submitHandler = { submitHandler }
+        changeEmail = { changeEmail }
+        email = { email }
+        alertEM = { alertEM }
+      />
+    </>
+  );
+};
+
+export default ForgetPW;
+
+
+//   //fetch data to send it to backend
+  //   fetch('http://localhost:4000/reset_password', {
+  //     method: "POST",
+  //     headers: {'Context-Type': 'application/json'},
+  //     body: JSON.stringify(data)
+  //     //in backend the data will be received in req.body
+  //   })
+  //   .then(res=>res.json())
+  //   .then(emailReceived=>console.log(emailReceived))
+  //   .catch(err=>console.log(err))
+  //
