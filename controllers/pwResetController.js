9b8b06a5fb958d4d1c9552c78c60015713c84b11
cybersagittarius <cybersagittarius@gmail.com>
2021-05-12 16:06:43 +0200

    wrote down the logic

---
+++ controllers/pwResetController.js
@@ -0,0 +1,14 @@
+//1st create route for check the token
+
+//2nd check the token from the req.body
+
+//3rd verify teh token with jwt.verify 
+
+//4tha you get the user and the id, and check in the colletion essential for the user OR
+//4thb use findOneAndUpdate by searching with the token
+
+//5th if you don't find the user then return status 400 saying wrong tokenb
+
+//6th if you find the user then return with everything is fine
+
+
