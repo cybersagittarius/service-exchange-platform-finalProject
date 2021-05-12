358c93de66a043450475a8651df4a4cf768d6c0f
cybersagittarius <cybersagittarius@gmail.com>
2021-05-12 09:35:47 +0200

    added Header and Main Page compoment in line 17 and 18

--- client/src/Components/Routing/Routes.js
+++ client/src/Components/Routing/Routes.js
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import SearchProvider from '../../context/SearchProvider';
+import MainPage from '../Main'
 import LandingPage from '../LandingPage';
 import LoginPage from '../userAccount/Login';
 import RegisterPage from '../userAccount/Register';
 import ContactPage from '../Contact';
 import VisitorsPage from '../Visitors';
-import NotFound from '../NotFound'
+import NotFoundPage from '../NotFound'
 import Footer from '../Footer'
 import Header from '../Header'
 
@@ -13,14 +14,16 @@ import Header from '../Header'
 const Routes = () => ( 
     <SearchProvider> 
         <Router>
+            <Header/>
+                <MainPage/>
                 <Switch>
                 <Route path="/" exact component={LandingPage}/>
                 <Route path="/login" exact component={LoginPage}/>
                 <Route path="/register" exact component={RegisterPage}/>
                 <Route path="/contact" exact component={ContactPage}/>
                 <Route path="/visitors" exact component={VisitorsPage}/>
-                <Route component={NotFound}/>
-            </Switch>
+                <Route component={NotFoundPage}/>
+            </Switch>            
             <Footer/>
         </Router>
     </SearchProvider>
