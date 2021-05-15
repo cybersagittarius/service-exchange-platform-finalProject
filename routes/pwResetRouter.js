b202cf89ac091728a2a82deb6bcba95ab104d33a
cybersagittarius <cybersagittarius@gmail.com>
2021-05-15 22:33:36 +0200

    added auth and two methods

--- routes/pwResetRouter.js
+++ routes/pwResetRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const pwResetController = require('../controllers/pwResetController')
+const auth = require('../middleware/auth')
+const pwResetController = require('../controllers/pwResetController');
+
+router.route('/:id/:token').get(auth, pwResetController)
 
 router.route('/').post(pwResetController)
-
 
 module.exports = router
