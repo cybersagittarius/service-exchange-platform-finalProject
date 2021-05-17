7b89c5a2eda4e632a45e526afcfa840082f76c0a
cybersagittarius <cybersagittarius@gmail.com>
2021-05-17 15:50:53 +0200

    added auth

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth')
 const loginController = require('../controllers/loginController')
 
-router.route('/').post(loginController)
+router.route('/').post(auth, loginController)
 
 module.exports = router
