c2279dc519e38f9a819cea77da6e7fc38bcb7a42
serendipity80-dev <ljiljana.bogdanovic@yahoo.com>
2021-05-17 14:27:14 +0200

    changes on the Visitor page

--- client/src/Components/Visitor.jsx
+++ client/src/Components/Visitor.jsx
@@ -1,104 +1,49 @@
 import React from 'react';
-import {Card, Button} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
+import Users from './UsersUI';
+import user1 from '../assets/img/user1.jpg';
+import user2 from '../assets/img/user2.jpg';
+import user3 from '../assets/img/user3.jpg';
+import user4 from '../assets/img/user4.jpg';
+import user7 from '../assets/img/user7.jpg';
+import user6 from '../assets/img/user6.jpg';
 
-function Visitor() {
-    const cardStyle = {
-        width:'18rem',
-        margin:'1rem 1.5rem',
-        borderRadius:'10px',
-        boxShadow:'0 5px 15px rgba(0,0,0,0.3)',
-        cursor:'pointer'
-    }
-    console.log("Hello there")
-    return (
-        <>
-        <div id="visitor">
-        <div className="title">
-           <h3>Show Case</h3> 
-       <Link to="/register">
-           <Button variant="outline-danger" block className=" visitor"> Join</Button>
-       </Link>
-       </div>
-       <div className="underline"></div>
 
-        <div className="showCase">
-        <Card style={cardStyle}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Oliver Smith</Card.Title>
-    <Card.Text>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui sunt beatae accusantium accusamus ratione recusandae aspernatur molestias, commodi dicta exercitationem.
-    </Card.Text>
-    <Link to="/notfound">
-    <Button variant="outline-danger" block>More Info</Button>
-    </Link>
-  </Card.Body>
-</Card>
-   <Card style={cardStyle}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Charlotte Johnson</Card.Title>
-    <Card.Text>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui sunt beatae accusantium accusamus ratione recusandae aspernatur molestias, commodi dicta exercitationem.
-    </Card.Text>
-    <Link to="/notfound">
-    <Button variant="outline-danger" block>More Info</Button>
-    </Link>
-  </Card.Body>
-</Card>
-   <Card style={cardStyle}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Liam Williams</Card.Title>
-    <Card.Text>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui sunt beatae accusantium accusamus ratione recusandae aspernatur molestias, commodi dicta exercitationem.
-    </Card.Text>
-    <Link to="/notfound">
-    <Button variant="outline-danger" block>More Info</Button>
-    </Link>
-  </Card.Body>
-</Card>
-       <Card style={cardStyle}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Emma Brown</Card.Title>
-    <Card.Text>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui sunt beatae accusantium accusamus ratione recusandae aspernatur molestias, commodi dicta exercitationem.
-    </Card.Text>
-    <Link to="/notfound">
-    <Button variant="outline-danger" block>More Info</Button>
-    </Link>
-  </Card.Body>
-</Card>
-   <Card style={cardStyle}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>David Garcia</Card.Title>
-    <Card.Text>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui sunt beatae accusantium accusamus ratione recusandae aspernatur molestias, commodi dicta exercitationem.
-    </Card.Text>
-    <Link to="/notfound">
-    <Button variant="outline-danger" block>More Info</Button>
-    </Link>
-  </Card.Body>
-</Card>
-   <Card style={cardStyle}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Nora Davis</Card.Title>
-    <Card.Text>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui sunt beatae accusantium accusamus ratione recusandae aspernatur molestias, commodi dicta exercitationem.
-    </Card.Text>
-    <Link to="/notfound">
-    <Button variant="outline-danger" block>More Info</Button>
-    </Link>
-  </Card.Body>
-</Card>
-</div>
+const Visitor = () => {
+  return (
+    <>
+ <div className="visitor">
+<h3>Show Category</h3>
+<Link to="/register">
+  <button className="btn btn-outline-danger btn-block">Join</button>
+</Link> 
  </div>
-</>
-    )
+<div className="underline"></div>
+ {/*start Card  */}
+    <div className="container-fluid d-flex justify-content-center">
+    <div className="row">
+      <div className="col-md-4">
+      <Users images={user1} title="Charlotte Johnson"/>
+      </div>
+      <div className="col-md-4">
+      <Users images={user2} title="Liam Williams"/>
+      </div>
+      <div className="col-md-4">
+      <Users images={user3} title="Emma Brown"/>
+      </div>
+      <div className="col-md-4">
+      <Users images={user4} title="Oliver Smith"/>
+      </div>
+      <div className="col-md-4">
+      <Users images={user7} title="Nora Davis"/>
+      </div>
+      <div className="col-md-4">
+      <Users images={user6} title="David Garcia"/>
+      </div>
+    </div>
+    </div>
+    </>
+  )
 }
 
 export default Visitor
