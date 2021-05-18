58961b7bf89421763fd8f64905db9a08e80d66ef
cybersagittarius <cybersagittarius@gmail.com>
2021-05-18 16:39:51 +0200

    changed state name to camelCase

--- client/src/Components/userControl/forms/PwResetForm.jsx
+++ client/src/Components/userControl/forms/PwResetForm.jsx
@@ -37,7 +37,7 @@ const PwResetForm = (props) => {
                         ref={passwordRef}
                         placeholder="password"
                         className="form-control"
-                        value={props.newpassword}
+                        value={props.newPassword}
                         onChange={props.changeNewPW}
                         required
                       />
