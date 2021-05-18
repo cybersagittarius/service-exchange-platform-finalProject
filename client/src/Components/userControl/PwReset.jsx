b1592fa97b4eea9d69cee932e7e176597754bcbf
cybersagittarius <cybersagittarius@gmail.com>
2021-05-18 16:34:43 +0200

    axiso function reworked, unfinished

--- client/src/Components/userControl/PwReset.jsx
+++ client/src/Components/userControl/PwReset.jsx
@@ -1,42 +1,29 @@
-import React, { useState} from 'react'
+import React, { useState, useContext } from 'react'
 import axios from 'axios';
+import searchContext from '../../context/SearchContext'
 
 import PwResetForm from './forms/PwResetForm'
 
 const PwReset = () => {
 
-    const [newpassword, setNewPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
     const [confirmNewPW, setConfirmNewPW] = useState("");
 
     const [alertPW, setAlertPW] = useState(false);
     const [alertPWCheck, setAlertPWCheck] = useState(false);
 
-    const getToken = async(req, res, err) => {
-        const token = req.params.token
-        console.log(token);
+    const context = useContext(searchContext);
+    const {userInfo} = context;
 
-        await axios.get('http://localhost:4000/:id/:token', token)
-        .then(res=>{
-            console.log('res received', res.data)
-        })
-        .then(canResetPassword => console.log(canResetPassword))
-        .catch(err=> console.log(err && err.response && err.response.data))
+     const changeNewPW = (e) => {
+        setNewPassword(e.target.value);
     }
-   
-    const resetPW = async(newpassword, confirmNewPW) => {
-        const data = {newpassword, confirmNewPW};
-        
-        console.log(data);
 
-        await axios.post('http://localhost:4000/reset_password', data)
-        .then(res=>{
-            console.log('res received, res.data')
-        })
-        .then(newPWReset => console.log(newPWReset))
-        .catch(err => console.log(err && err.response && err.response.data))
-       }
-
-    const submitHandler = (e)=> {
+    const changeConfirmNewPW = (e) =>{
+        setConfirmNewPW(e.target.value)
+    };
+    
+    const submitHandler = (e) => {
         e.preventDefault();
 
         // const pwValidator = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])([a-zA-Z0-9@$!%*?&]{8,12})$/
@@ -50,7 +37,7 @@ const PwReset = () => {
         //     return false;                    
         // }        
 
-        if(newpassword !== confirmNewPW) {
+        if(newPassword !== confirmNewPW) {
             setAlertPWCheck(true)
             setTimeout(()=>{
                 setAlertPWCheck(false)
@@ -58,15 +45,22 @@ const PwReset = () => {
             return false;
         }
 
-        !getToken()? console.log('Error!') :resetPW(newpassword, confirmNewPW)
-    }
-    
-    const changeNewPW = (e)=> {
-        setNewPassword(e.target.value);
-    }
+        const url = `http://localhost:4000`
+                const authAxios = axios.create({
+                baseURL: url,
+                headers: { Authorization: `Bearer ${userInfo}`, 
+                           "Content-Type": "application/json" },  
+                data: { newPassword }                        
+            }) 
 
-    const changeConfirmNewPW = (e) =>{
-        setConfirmNewPW(e.target.value);
+        const updatePW = async ()=>{
+                try{
+                    const result = await authAxios.post('/reset_password')
+                    console.log(result.data)
+                } catch(err)
+                {console.log(err.message)}
+            }
+            updatePW();           
     }
 
     return (
@@ -87,4 +81,71 @@ const PwReset = () => {
     )
 }
 
+
 export default PwReset
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // const submitHandler = (e)=> {
+    //     e.preventDefault();
+
+        // const pwValidator = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])([a-zA-Z0-9@$!%*?&]{8,12})$/
+        // const isPwValid = pwValidator.test(newpassword);
+
+        // if(!isPwValid){
+        //     setAlertPW(true)
+        //     setTimeout(() => {
+        //         setAlertPW(false)
+        //     }, 20000); 
+        //     return false;                    
+        // }        
+
+    //     if(newpassword !== confirmNewPW) {
+    //         setAlertPWCheck(true)
+    //         setTimeout(()=>{
+    //             setAlertPWCheck(false)
+    //         }, 5000);
+    //         return false;
+    //     }
+
+     
+    // }
+    
+    // const changeNewPW = (e)=> {
+    //     setNewPassword(e.target.value);
+    // }
+
+    // const changeConfirmNewPW = (e) =>{
+    //     setConfirmNewPW(e.target.value);
+    // }
