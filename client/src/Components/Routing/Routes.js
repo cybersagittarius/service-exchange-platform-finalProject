eb24c2a735aae95bbdda531c93bb5820e2e21c46
cybersagittarius <cybersagittarius@gmail.com>
2021-05-26 13:48:07 +0200

    added chat router

--- client/src/Components/Routing/Routes.js
+++ client/src/Components/Routing/Routes.js
@@ -10,9 +10,9 @@ import NotFoundPage from '../NotFound'
 import Footer from '../Footer'
 import Header from '../Header'
 import SearchPage from '../Search';
-import ForgetPW from '../userControl/ForgetPW';
-import PwReset from '../userControl/PwReset_copy';
-import Chat from '../chat/Chat';
+import ForgetPWPage from '../userControl/ForgetPW';
+import PwResetPage from '../userControl/PwReset_copy';
+import ChatPage from '../chat/Chat';
 
 
 const Routes = () => ( 
@@ -23,13 +23,13 @@ const Routes = () => (
                 <Route path="/" exact component={LandingPage}/>
                 <Route path="/login" exact component={LoginPage}/>
                 <Route path="/register" exact component={RegisterPage}/>
-                <Route path="/forget_password" exact component={ForgetPW}/>
-                <Route path="/reset_password/:token" exact component={PwReset}/> 
+                <Route path="/forget_password" exact component={ForgetPWPage}/>
+                <Route path="/reset_password/:token" exact component={PwResetPage}/> 
                 <Route path="/contact" exact component={ContactPage}/>
                 <Route path="/visitors" exact component={VisitorsPage}/>
                 <Route path="/not_found" exact component={NotFoundPage}/>
                 <Route path="/search" exact component={SearchPage}/>
-                {/* <Route path="/chat" exact component={Chat} /> */}
+                <Route path="/chat" exact component={ChatPage} />
             </Switch>            
             <Footer/>
         </Router>
