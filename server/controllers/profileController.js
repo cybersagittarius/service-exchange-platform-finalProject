13e64dc16192f1c80afc83d5076840bd3ce8b395
cybersagittarius <cybersagittarius@gmail.com>
2021-05-28 11:45:37 +0200

    line 16 see comment

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -4,12 +4,17 @@ const express = require("express");
 //const session = request('express-session');
 
 const profileUser= async (req, res, next) => {  
-  
+  // we only update
+  //firstname, lastname, country, region, username, skills, avatar
+  // we post   description
+  // email is not to be modified
+
   console.log(req.user)
   
   try {
-    //id: user._id  
-    const user = await User.findByIdAndUpdate(req.user.id, req.body, {new: true})      
+    //id: user._id. req.user._id, instead of req.user.id is used, because in login controller the payload takes _id as one part of the jwt generator                                        
+    const user = await User.findByIdAndUpdate(req.user._id, req.body, {new: true}) 
+    //const user = await User.findOneAndUpdate({id: req.user._id}, {new: true})      
 
     if (!user) {
       return res.status(400).json({ errors: { msg: "no user found!" } });
