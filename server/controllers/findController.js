bcbe95c3dbb62901fddb3b29c3db5047986ed22d
cybersagittarius <cybersagittarius@gmail.com>
2021-05-31 10:57:21 +0200

    started working on findRouter and Controller

--- server/controllers/findController.js
+++ server/controllers/findController.js
@@ -1,9 +1,18 @@
 const express = require ('express');
 const User = require('./models/userModel')
+const Essential = require ('./models/Essential')
+
+const findController = (req, res, next) => {
+    const { searchSkills, searchCountry, searchRigion } = req.body;
+    const user = {searchSkills, searchCountry, searchRigion}
+    
+    
+
+    const findSkills = () =>{
+        
+    }
 
 
-
-
-
+}
 
 module.exports = findController
--- server/routes/findRouter.js
+++ server/routes/findRouter.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const findController = require('../controllers/findController')
 
-router.route('/').get(findController)
+
         
 router.route('/').post(findController)
 
