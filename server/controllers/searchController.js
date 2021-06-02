bd7f86f0974d3d1a42daf00e33fd07f84a369f96
cybersagittarius <cybersagittarius@gmail.com>
2021-06-02 13:55:14 +0200

    came up with two initial versions, 2 functions or just 1?

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -1,18 +1,56 @@
 const express = require ('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
 const User = require('./models/userModel')
-const Essential = require ('./models/Essential')
 
-const searchController = (req, res, next) => {
-    const { searchSkills, searchCountry, searchRigion } = req.body;
-    const user = {searchSkills, searchCountry, searchRigion}
+const searchController = async(req, res, next) => {
+    const { token } = req.body.token;
+    if(token===null){
+        //const { lookselection, country, region } = req.body
+        await res.status(401).json({msg: "return limited search"})
+        } else {
+            const verified = jwt.verify(token, process.env.JWT_SECRET);
+            if(!verified){
+                await res.status(401).json({msg: "returne limited search"})
+                }else{
+                    await res.status(200).json({msg: "return full search"})
+                    } 
+                }
+            }
     
-    
+  module.exports = searchController
+// const limitedSearch = async(req, res, next) => {
+//     const { token } = req.body.token;
+//     if(token===null){
+//         //const { lookselection, country, region } = req.body
+//         res.status(401).json({msg: "return limited search"})
+//     }
+// }
 
-    const findSkills = () =>{
-        
-    }
+// const fullSearch = async(req, res, next) => {
+//     const { token } = req.body.token;
+//     if(token!==null){
+//         const verified = jwt.verify(token, process.env.JWT_SECRET);
+//         if(!verified){
+//             res.status(401).json({msg: "returne limited search"})
+//         }
+//         else{
+//             res.status(200).json({msg: "return full search"})
+//         } 
+//     }
+// }
+
+// module.exports = { limitedSearch, fullSearch }
+
+//1. takes 2 function  searchController.limitedSearch searchController.fullSearch
+// receive data from frontend to check the identity of user
+//if, else =>first check if the user is auth
+//2. no auth, limitedSearch() => 
+     // get the data from the database    
+     //generate the limited search outcome, 
+//3. with auth fullSearch() =>
+    // get the data from the database
+    // generate the full outcome
 
 
-}
 
-module.exports = searchController
