1ab9434e0fcf54c5e8a6e7de80f9d2a1339c33e4
cybersagittarius <cybersagittarius@gmail.com>
2021-06-02 16:26:34 +0200

    finished 1st draft

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -1,46 +1,59 @@
 const express = require ('express');
-const router = express.Router();
 const jwt = require('jsonwebtoken');
-const User = require('./models/userModel')
+const User = require('../models/userModel')
 
-const searchController = async(req, res, next) => {
-    const { token } = req.body.token;
-    if(token===null){
-        //const { lookselection, country, region } = req.body
-        await res.status(401).json({msg: "return limited search"})
+const checkToken4Search = async(req, res, next) => {
+    //look at auth again to determine where the token comes from, 
+    //the previous auth file has this //&& authHeader.split(' ')[1];
+    //it was causing an issue, now commented now
+
+    //we do not do { token } because we are not destructuring, there is only one set of info to be assgined to req.headers.token
+    const token = req.headers.token;
+    //(token===null) threw an error because token is undefine. Better do !token
+    if(!token){
+        await limitedSearch(req, res, next)
         } else {
-            const verified = jwt.verify(token, process.env.JWT_SECRET);
-            if(!verified){
-                await res.status(401).json({msg: "returne limited search"})
-                }else{
-                    await res.status(200).json({msg: "return full search"})
-                    } 
-                }
-            }
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        if(!verified){
+            await limitedSearch(req, res, next)
+        }else{
+            await fullSearch(req, res, next)
+        } 
+    }
+}
+
+const limitedSearch = (req, res, next) => {
+    // we want to find all users with the same lookselection and country
+    const { lookselection, country, region } = req.body
+
+    // get id from lookselection
+    let arrayOfSkillsIds = lookselection.map(item=>item.id);
+
+    // take the Ids and the country to search for users
+    // to do this we will use the user model and the find method
+    // User.find({condition},{config})
+    User.find({country: country, region: region, "skills.id": {$in: arrayOfSkillsIds} }, {_id:0, email:0, firsname:0, lastname:0})
+    .then(data=>{
+        res.json(data)
+    }).catch(error=>{
+        res.send(error.message)
+    })      
+}
+
+const fullSearch = async(req, res, next) => {
+    const { lookselection, country, region } = req.body
+
+    let arrayOfSkillsIds = lookselection.map(skill=>skill.id);
     
-  module.exports = searchController
-// const limitedSearch = async(req, res, next) => {
-//     const { token } = req.body.token;
-//     if(token===null){
-//         //const { lookselection, country, region } = req.body
-//         res.status(401).json({msg: "return limited search"})
-//     }
-// }
+    User.find({country: country, region: region, "skills.id": {$in: arrayOfSkillsIds} }, {_id:0})
+    .then(data=>{
+        res.json(data)
+    }).catch(error=>{
+        res.send(error.message)
+    })   
+}
 
-// const fullSearch = async(req, res, next) => {
-//     const { token } = req.body.token;
-//     if(token!==null){
-//         const verified = jwt.verify(token, process.env.JWT_SECRET);
-//         if(!verified){
-//             res.status(401).json({msg: "returne limited search"})
-//         }
-//         else{
-//             res.status(200).json({msg: "return full search"})
-//         } 
-//     }
-// }
-
-// module.exports = { limitedSearch, fullSearch }
+module.exports = checkToken4Search 
 
 //1. takes 2 function  searchController.limitedSearch searchController.fullSearch
 // receive data from frontend to check the identity of user
