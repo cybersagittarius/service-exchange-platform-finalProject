1e95e851ca056d88bce010178f7f7717a839432b
cybersagittarius <cybersagittarius@gmail.com>
2021-06-02 16:25:52 +0200

    finished 1st draft

--- server/routes/searchRouter.js
+++ server/routes/searchRouter.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const searchController = require('../controllers/searchController')
-        
+
 router.route('/').post(searchController)
 
-module.export = router
+module.exports = router
