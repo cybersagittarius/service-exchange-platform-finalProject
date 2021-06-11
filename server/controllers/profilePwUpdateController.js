f851dfc76029ebb8a8ff6e13165b3f7ebfb98315
cybersagittarius <cybersagittarius@gmail.com>
2021-06-11 10:40:15 +0200

    finished profile password update

---
+++ server/controllers/profilePwUpdateController.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const Essential = require('../models/essentialModel');
+
+const profilePWChange = async(req, res, next) => {
+
+  let { email } = req.user
+  let essential = await Essential.findOne({email: email}) 
+
+  try {
+    if(!essential) {
+      return res.json({msg: 'no user found!', status: 404})
+    } 
+    //get the password from req.body
+    let password = req.body.password
+    //tell the model to put this into the database
+    essential.password = password 
+    //save the newly updated pw to the database
+    await essential.save()
+    //tell user that it was successful
+    return res.json({msg: "updated as you wish!", status: 200})
+    
+  } catch (error) {
+     return res.json({msg: error, status: 500})
+  }
+}
+
+module.exports = profilePWChange
+ 
+
+
--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -1,6 +1,201 @@
 const express = require ('express');
-const jwt = require("jsonwebtoken");
+//const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
+
+// version not working up to now
+// const checkToken4Search = async(req, res, next) => {
+
+//     const { country, region, lookSelection, userInfo } = req.body
+
+//     const token = userInfo.token
+//     const arrayOfSkillsIds = lookSelection.map(item => item.id);
+
+    // if(!token){
+    //     limitedSearch(res, next)
+    // }else if(token){
+    //     fullSearch(res, next)
+    // }
+
+    
+    // const limitedSearch = async(res, next) => {
+    //    try {
+    //     if(!token){
+    //         console.log("only limited search allowed because there is no token")            
+
+    //         const limitedResults = await User.find({country: country, region: region, "skills.id": {$in: arrayOfSkillsIds} }, {_id:false, email:false, firstname:false, lastname:false, hobby: false})
+
+    //         res.status(200).json({limitedResults}) }
+    // } catch (error) {
+    //     next(error.message)
+    // }
+
+    // const fullSearch = async(res, next) => {
+    //     try {
+    //         console.log("full search allowed")            
+
+    //         const fullResults = await User.find({country: country, "skills.id": {$in: arrayOfSkillsIds} }, {_id:0})
+    //         res.status(200).json({fullResults})
+    //     } catch (error) {
+    //         next(error.message)
+    //         }         
+    //     } 
+    // }
+// }      
+
+// module.exports = checkToken4Search 
+
+
+
+const checkToken4Search = async(req, res, next) => {
+     
+    // we do not do { token } because we are not destructuring, there is only one set of info to be assgined to req.headers.token
+                                    // let token = null;
+                                    // if(req.headers.authorization){
+                                    //     token = req.headers.authorization.split(" ")[1]
+                                    // }
+    // there's a bearer token in here
+    // Bearer + token   
+    // const token = req.headers.token
+    
+    //(token===null) threw an error because token is undefine. Better do !token
+
+                                    const {userInfo} = req.body
+                                    const token = userInfo.token
+
+    if(!token){
+        console.log("only limited search allowed because there is no token")
+        await limitedSearch(req, res, next)
+    } else {
+                console.log("full search allowed")
+                console.log(token)
+               await fullSearch(req, res, next)
+            }
+        };    
+ 
+
+const limitedSearch = (req, res, next) => {
+    // we want to find all users with the same lookselection and country
+    console.log(req.body)
+    const { lookSelection, country, region, userInfo, searchResults } = req.body //decide if we should include region later
+
+    // get id from lookselection
+    let arrayOfSkillsIds = lookSelection.map(item=>item.id);
+
+    // take the Ids and the country to search for users
+    // to do this we will use the user model and the find method
+    // User.find({condition},{config})
+    //region: region, we can decide if we want to include them after country: country
+    User.find({country: country, region: region, "skills.id": {$in: arrayOfSkillsIds} }, {_id:false, email:false, firstname:false, lastname:false, hobby: false})
+    .then(data=>{
+        console.log(data)
+        res.json(data)
+    }).catch(error=>{
+        res.send(error.message)
+    })      
+}
+
+const fullSearch = (req, res, next) => {
+    //take out region from deconstructuring for now
+    console.log(req.body)
+    const { lookSelection, country, region, userInfo, searchResults } = req.body    
+
+    let arrayOfSkillsIds = lookSelection.map(item=>item.id);
+
+    //take out region: region after country: country for now
+    User.find({country: country, region: region, "skills.id": {$in: arrayOfSkillsIds} }, {_id:0})
+    .then(data=>{
+        console.log(data)
+        res.json(data)
+    }).catch(error=>{
+        res.send(error.message)
+    }) 
+}
+
+module.exports = checkToken4Search 
+
+
+
+
+
+// Version with Sebastian
+
+// const checkToken4Search = async(req, res, next) => {
+
+//     // look at auth again to determine where the token comes from, 
+//     // the previous auth file has this //&& authHeader.split(' ')[1];
+//     // it was causing an issue, now commented out
+    
+//     // we do not do { token } because we are not destructuring, there is only one set of info to be assgined to req.headers.token
+//                                     // let token = null;
+//                                     // if(req.headers.authorization){
+//                                     //     token = req.headers.authorization.split(" ")[1]
+//                                     // }
+//     // there's a bearer token in here
+//     // Bearer + token   
+//     // const token = req.headers.token
+    
+//     //(token===null) threw an error because token is undefine. Better do !token
+
+//                                     const {userInfo} = req.body
+//                                     const token = userInfo.token
+
+//     if(!token){
+//         console.log("only limited search allowed because there is no token")
+//         await limitedSearch(req, res, next)
+//     } else {
+//         jwt.verify(token, process.env.JWT_SECRET, (err, decoded)=>{
+//             if(err){
+//                 console.log("only limited search allowed because the token is not valid")
+//                 limitedSearch(req, res, next)
+//             }else{
+//                 console.log("full search allowed")
+//                 console.log(token)
+//                 fullSearch(req, res, next)
+//             } 
+//         });
+//     }
+//  }
+
+// const limitedSearch = (req, res, next) => {
+//     // we want to find all users with the same lookselection and country
+//     console.log(req.body)
+//     const { lookSelection, country, region, userInfo, searchResults } = req.body //decide if we should include region later
+
+//     // get id from lookselection
+//     let arrayOfSkillsIds = lookSelection.map(item=>item.id);
+
+//     // take the Ids and the country to search for users
+//     // to do this we will use the user model and the find method
+//     // User.find({condition},{config})
+//     //region: region, we can decide if we want to include them after country: country
+//     User.find({country: country, region: region, "skills.id": {$in: arrayOfSkillsIds} }, {_id:false, email:false, firstname:false, lastname:false, hobby: false})
+//     .then(data=>{
+//         console.log(data)
+//         res.json(data)
+//     }).catch(error=>{
+//         res.send(error.message)
+//     })      
+// }
+
+// const fullSearch = (req, res, next) => {
+//     //take out region from deconstructuring for now
+//     console.log(req.body)
+//     const { lookSelection, country, region, userInfo, searchResults } = req.body    
+
+//     let arrayOfSkillsIds = lookSelection.map(item=>item.id);
+
+//     //take out region: region after country: country for now
+//     User.find({country: country, region: region, "skills.id": {$in: arrayOfSkillsIds} }, {_id:0})
+//     .then(data=>{
+//         console.log(data)
+//         res.json(data)
+//     }).catch(error=>{
+//         res.send(error.message)
+//     }) 
+// }
+
+// module.exports = checkToken4Search 
+
 
 // const checkToken4Search = async(req, res, next) => {
    
@@ -55,75 +250,6 @@ const User = require('../models/userModel')
     // generate the full outcome
 
 
-// Version with Sebastian
-const checkToken4Search = async(req, res, next) => {
-    //look at auth again to determine where the token comes from, 
-    //the previous auth file has this //&& authHeader.split(' ')[1];
-    //it was causing an issue, now commented out
-    
-    //we do not do { token } because we are not destructuring, there is only one set of info to be assgined to req.headers.token
-    let token = null;
-    if(req.headers.authorization){
-        token = req.headers.authorization.split(" ")[1]
-    }
-    //there's a bearer token in here
-    //Bearer + token   
-    //const token = req.headers.token
-    
-    //(token===null) threw an error because token is undefine. Better do !token
-    if(!token){
-        console.log("only limited search allowed because there is no token")
-        await limitedSearch(req, res, next)
-    } else {
-        jwt.verify(token, process.env.JWT_SECRET, (err,decoded)=>{
-            if(err){
-                console.log("only limited search allowed because the token is not valid")
-                limitedSearch(req, res, next)
-            }else{
-                console.log("full search allowed")
-                fullSearch(req, res, next)
-            } 
-        });
-    }
-}
-
-const limitedSearch = (req, res, next) => {
-    // we want to find all users with the same lookselection and country
-    console.log(req.body)
-    const { lookselection, country } = req.body //decide if we should include region later
-
-    // get id from lookselection
-    let arrayOfSkillsIds = lookselection.map(item=>item.id);
-
-    // take the Ids and the country to search for users
-    // to do this we will use the user model and the find method
-    // User.find({condition},{config})
-    //region: region, we can decide if we want to include them after country: country
-    User.find({country: country, "skills.id": {$in: arrayOfSkillsIds} }, {_id:false, email:false, firstname:false, lastname:false, hobby: false})
-    .then(data=>{
-        res.json(data)
-    }).catch(error=>{
-        res.send(error.message)
-    })      
-}
-
-const fullSearch = (req, res, next) => {
-    //take out region from deconstructuring for now
-    console.log(req.body)
-    const { lookselection, country } = req.body    
-
-    let arrayOfSkillsIds = lookselection.map(item=>item.id);
-
-    //take out region: region after country: country for now
-    User.find({country: country, "skills.id": {$in: arrayOfSkillsIds} }, {_id:0})
-    .then(data=>{
-        res.json(data)
-    }).catch(error=>{
-        res.send(error.message)
-    })   
-}
-
-module.exports = checkToken4Search 
 
 
 
