1b9572eb0638e8bde859d4bc3630589316e27d2c
Majuszka <majamarek.design@gmail.com>
2021-06-11 11:07:47 +0200

    some changes

---
+++ client/src/Components/LogInComp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from './userControl/Login'
+import Main from './Main';
+
+const LogInComp = () => {
+    return (
+        <>
+         <Login/>  
+         <Main/>
+        </>
+    );
+}
+
+export default LogInComp;
